080d40984ee671f75de3bef255bd0e878a57d545
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-04-14 14:48:18 +0200

    Move api to fetchData

---
+++ src/api/fetchData.js
@@ -0,0 +1,36 @@
+import { loginApi } from "."
+import { authUserApi } from "."
+
+export const logIn = (form) => {
+    const options = { 
+        method: 'POST', 
+        body: JSON.stringify(form),
+        headers: {"Content-Type": "application/json"}
+    }
+
+    return _fetch(loginApi, options)
+
+}
+
+export const authorize = () => {
+    const token = sessionStorage.getItem("token");
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'authorization': token
+        }
+    }
+     return _fetch(authUserApi, options)
+}
+
+const _fetch = (url, options) => {
+    return fetch(url, options)
+        .then(res =>{
+            if(!res.ok){
+                throw Error(res.statusText)
+            }
+            return res.json()
+        }) 
+} 
