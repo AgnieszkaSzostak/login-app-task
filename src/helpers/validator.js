204f6db79cbe03163d1de209d8076cb6d41c5144
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-04-15 17:08:40 +0200

    Change errors

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,19 +1,19 @@
 import { fields } from "./fields";
 
 export const validateForm = (form) => {
-    let errors = [];
+    let errors = null;
     fields.forEach(field => {
         const {label, name, required, pattern, error} = field;
         const value = form[name];
         if(required){
             if(value.length === 0){
                 const error = `${label} is required!`
-                errors = [...errors, `${error}`]
+                errors = {...errors, [name]:`${error}`}
             } 
         }else if(pattern){
             const reg = new RegExp(pattern)
             if(!reg.test(value)){
-                errors = [...errors, `${error}`]
+                errors = {...errors, [name]:`${error}`}
             }
         }
     })
