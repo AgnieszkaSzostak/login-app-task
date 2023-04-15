a6a58e8903ab11e244c174e2aed62c9cb0beb33e
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-04-15 17:08:07 +0200

    Render Field component & render LoginForm conditionally

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 import { validateForm } from "../helpers/validator";
 import { authorize, logIn } from "../api/fetchData";
 import StyledForm from "./StyledLoginForm.styled";
+import Field from "./Field";
+import Error from "./Error";
 
 const LoginForm = (props) => {
     const {setLoggedUser} = props;
@@ -16,15 +18,16 @@ const LoginForm = (props) => {
     })
 
     useEffect(() => {
+        if( validationErrors && validationErrors.email){
+            setSubpage(1);
+        }
+    },[validationErrors, setSubpage])
+
+    useEffect(() => {
         if(isLogged){
             authorize()
                 .then(resp => setLoggedUser(resp.fullUserName))
                 .catch(err => console.error('errAuth', err))
-                .finally(setForm({ 
-                    email: '',  
-                    userName: '',
-                    password: '',
-                }))
         }
     }, [isLogged, setLoggedUser])
 
@@ -44,13 +47,15 @@ const LoginForm = (props) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const errors = validateForm(form);
-        errors.length > 0  
-        ? setValidationErrors(errors) 
+        console.log(form);
+        errors !== null 
+        ? setValidationErrors(errors)
         : logIn(form)
             .then(data => {
                 sessionStorage.setItem("token", data.token);
                 setIsLogged(true);
             })
+            .then()
             .catch(err => {
                 console.error('errLogin', err);
                 if(err.message === "Bad Request"){
@@ -59,37 +64,40 @@ const LoginForm = (props) => {
                     setError("Wrong credentials")
                 }
             })
+            .finally(()=> {
+                setForm({ 
+                    email: '',  
+                    userName: '',
+                    password: '',
+                });
+                setSubpage(1);
+            })
     }
-
+    if(isLogged) {
+        return null
+    }
     return (
         <>
+        
             <StyledForm className="form" onSubmit={handleSubmit}>
                 {subpage === 1 
                     ? 
                     (
                         <>  
-                            {/* <label>email</label> */}
-                            <input className="form__input" value={form.email} placeholder="email" type="email" name="email" onChange={(e) => handleChange(e)}></input>
+                            <Field value={form.email} name="email" type="email" label="email" onChange={e =>handleChange(e)} error={validationErrors ? validationErrors.email : null}/>
                             <button className="form__button form__button--next" onClick={handleClick}>Next</button>
                         </>
                     )
                     :
                     (
                         <>
-                            {/* <label>login</label> */}
-                            <input className="form__input" value={form.userName} placeholder="login" type="text" name="userName" onChange={(e) => handleChange(e)}></input>
-                            {/* <label>password</label> */}
-                            <input className="form__input" value={form.password} placeholder="password" type="password" name="password" onChange={(e) => handleChange(e)}></input>
+                            <Field value={form.userName} name="userName" type="text" label="login" onChange={e =>handleChange(e)} error={validationErrors ? validationErrors.userName : null}/>
+                            <Field value={form.password} name="password" type="password" label="password" onChange={e =>handleChange(e)} error={validationErrors ? validationErrors.password : null}/>
                             <input className="form__button form__button--submit" type="submit" value="submit"/>
                         </>
                     )}
             </StyledForm>
-            {validationErrors 
-                ? validationErrors.map((error, index) => <p key={index}>{error}</p>)
-                : null
-            }
-
-            {error && <p>{error}</p>}
+            {error && <Error message={error}></Error>}
         </>
     )
 }
