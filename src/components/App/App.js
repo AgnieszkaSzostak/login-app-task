5e66633d7b74b895e5e9dd5d8d891c894a0e57b2
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-05-09 17:09:22 +0200

    Add form state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,19 +5,17 @@ import Header from '../Header/Header';
 import Panel from '../Panel/Panel';
 import RegisterForm from '../Forms/RegisterForm';
 
+
 const App = () => {
-  const [registerPage, setRegisterPage] = useState(false);
   const [fullUserName, setFullUserName] = useState('');
   const token = sessionStorage.getItem('token');
-  console.log(token);
+  const [form, setForm] = useState(<LoginForm setLoggedUser={setFullUserName} token={token}/>);
+  
   return (
     <StyledApp className="App">
       <Header user={fullUserName}/>
-      {registerPage 
-        ? <RegisterForm/> 
-        : <LoginForm setLoggedUser={setFullUserName} token={token}/>
-      }
-      <Panel changeForm={setRegisterPage}/>
+      {form}
+      <Panel changeForm={setForm}/>
     </StyledApp>
   );
 }
