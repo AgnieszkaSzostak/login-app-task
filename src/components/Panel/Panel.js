315308edeafceb5c52a662066824579c5b3136f1
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-05-09 17:13:37 +0200

    Add state, render forms conditionaly, add panels.js, import panels

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,19 +1,31 @@
 import React from "react";
 import StyledPanel from "./StyledPanel";
+import LoginForm from "../Forms/LoginForm";
+import RegisterForm from "../Forms/RegisterForm";
+import Button from "../Button/Button";
+import { useState } from "react";
+import { panels } from "../../helpers/panels";
 
 const Panel = (props) => {
   const {changeForm} = props; 
-  const handleClick = () => {
-    changeForm(true);
+  const [state, setState] = useState('register');
+  const handleClick = (e) => {
+    if(state === 'register') {
+      setState('login') 
+      changeForm(<RegisterForm/>);
+    }else{
+      setState('register')
+      changeForm(<LoginForm/>)
+    }
   }
+  const {title, description, button} = panels[state]
     return (
         <StyledPanel className="panel">
               <div className="panel__content--overlay">
-                <h1 className="panel__title">Join our community!</h1>
-                <p className="panel__description">Register now and discover all the features of our app.</p>
-                <button onClick={handleClick}>Register</button>
+                <h1 className="panel__title">{title}</h1>
+                <p className="panel__description">{description}</p>
+                <Button onClick={handleClick}>{button}</Button>
               </div>
-
         </StyledPanel>
     )
 }
---
+++ src/helpers/panels.js
@@ -0,0 +1,12 @@
+export const panels = {
+    register: {
+        title: "Join our community!",
+        description: "Register now and discover all the features of our app.",
+        button: "Sign up"
+    },
+    login: {
+        title: "Already have account?",
+        description: "Sign in to your account",
+        button: "Login"
+    },
+}
